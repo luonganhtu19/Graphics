37f0d8f0a7ac839fac2f236e5910aed594e6257d
- <->
2020-11-16 11:38:35 +0700

    completeVr1

--- src/programMain.java
+++ src/programMain.java
@@ -1,9 +1,13 @@
+import subClass.Point3D;
 import superClass.Point2D;
 
 public class programMain {
     public static void main(String[] args) {
-        Point2D point2D=new Point2D(3,5);
+        Point2D point2D=new Point2D(3.5f, 5.78f);
         System.out.println(point2D);
+
+        Point3D point3D=new Point3D(1,1,5);
+        System.out.println(point3D);
     }
 
 
--- src/subClass/Point3D.java
+++ src/subClass/Point3D.java
@@ -1,13 +1,45 @@
 package subClass;
 
-import java.awt.geom.Point2D;
+import superClass.Point2D;
+
+import java.util.Arrays;
 
 public class Point3D extends Point2D {
     private float z=0;
+
     public Point3D(){};
+    public Point3D(float x,float y,float z){
+        super(x, y);
+        this.z=z;
+    }
+
+    public float getZ() {
+        return z;
+    }
+
+    public void setZ(float z) {
+        this.z = z;
+    }
+
+    public void setXYZ(float x,float y,float z){
+     setXY(x,y);
+     this.z=z;
+    }
+    public float[] getXYZ(){
+        float[] arr3=new float[3];
+        arr3[0]=getX();
+        arr3[1]=getY();
+        arr3[2]=getZ();
+        return arr3;
+    }
 
     @Override
     public String toString() {
-        return super.toString();
+        return "Point3D{" +
+                "x="+ getX()+
+                " y="+getY()+
+                " z=" + z +
+                " arrXYZ"+ Arrays.toString(getXYZ()) +
+                '}';
     }
 }
